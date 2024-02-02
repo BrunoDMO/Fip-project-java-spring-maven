99d3a978c42bfd95bba6d40fa434eb6d19d4b502
BrunoDMO <brunonaw41@gmail.com>
2024-02-02 16:34:13 -0300

    First commit

---
+++ src/main/java/portifolio/bruno/Fipprojectjavaspringmaven/FipProjectJavaSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package portifolio.bruno.Fipprojectjavaspringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipProjectJavaSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipProjectJavaSpringMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portifolio/bruno/Fipprojectjavaspringmaven/FipProjectJavaSpringMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package portifolio.bruno.Fipprojectjavaspringmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipProjectJavaSpringMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
